5c8637fba7e9b93193503e409453bfd75f263f11
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-17 08:57:03 +0400

    NEW: setup static build for server on start

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Cloning repos
 gh repo clone DesterLib/Server /workspaces/Server
 gh repo clone DesterLib/Frontend /workspaces/Frontend
 
+# Server setup
 pip3 install -r /workspaces/Server/requirements.txt
-cd /workspaces/Frontend && npm install && cd /workspaces
 
+# Frontend setup
+cd /workspaces/Frontend && npm install && npm run build && cd /workspaces
+mv /workspaces/Frontend/build /workspaces/Server/build
+
+# Rclone setup
 curl https://rclone.org/install.sh | sudo bash
