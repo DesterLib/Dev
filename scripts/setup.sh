6b666789f0aa83c6341133302138f9938e147710
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-17 08:44:40 +0400

    NEW: added extentions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,6 @@
 
 gh repo clone DesterLib/Server /workspaces/Server
 gh repo clone DesterLib/Frontend /workspaces/Frontend
+
+pip3 install -r /workspaces/Server/requirements.txt
+cd /workspaces/Frontend && npm install && cd /workspaces
