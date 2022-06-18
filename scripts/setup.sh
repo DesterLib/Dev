845d667061b05b1a251da80dd9c7aae1f1e583bc
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-18 17:24:25 +0400

    NEW: auto setup python venv

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ gh repo clone DesterLib/Server /workspaces/Server
 gh repo clone DesterLib/Frontend /workspaces/Frontend
 
 # Server setup
+python3 -m venv /workspaces/Server/.venv
+source /workspaces/Server/.venv/bin/activate
 pip3 install -r /workspaces/Server/requirements.txt
 
 # Frontend setup
