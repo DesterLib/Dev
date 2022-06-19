5e6262643c2bb54aed9d2a377224d73297ee20aa
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-19 13:39:27 +0400

    NEW: docs repository

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@
 # Cloning repos
 gh repo clone DesterLib/Server /workspaces/Server
 gh repo clone DesterLib/Frontend /workspaces/Frontend
+gh repo clone DesterLib/Docs /workspaces/Docs
 
 # Server setup
 python3 -m venv /workspaces/Server/.venv
@@ -12,6 +13,9 @@ pip3 install -r /workspaces/Server/requirements.txt
 # Frontend setup
 cd /workspaces/Frontend && npm install && npm run build && cd /workspaces
 mv /workspaces/Frontend/build /workspaces/Server/build
+
+# Docs setup
+cd /workspaces/Docs && npm install && cd /workspaces
 
 # Rclone setup
 curl https://rclone.org/install.sh | sudo bash
