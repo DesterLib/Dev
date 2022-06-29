11fdf046669226237c8f0cf32a5a1ab86d38c41e
Elias Benbourenane <eliasbenbourenane@gmail.com>
2022-06-29 10:49:00 +0000

    FIX: updated backend repo URL

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Cloning repos
-gh repo clone DesterLib/Server /workspaces/Server
+gh repo clone DesterLib/Backend /workspaces/Backend
 gh repo clone DesterLib/Frontend /workspaces/Frontend
 gh repo clone DesterLib/Docs /workspaces/Docs
 
-# Server setup
-python3 -m venv /workspaces/Server/.venv
-source /workspaces/Server/.venv/bin/activate
-pip3 install -r /workspaces/Server/requirements.txt
+# Backend setup
+python3 -m venv /workspaces/Backend/.venv
+source /workspaces/Backend/.venv/bin/activate
+pip3 install -r /workspaces/Backend/requirements.txt
 
 # Frontend setup
 cd /workspaces/Frontend && npm install && npm run build && cd /workspaces
-mv /workspaces/Frontend/build /workspaces/Server/build
+mv /workspaces/Frontend/build /workspaces/Backend/build
 
 # Docs setup
 cd /workspaces/Docs && npm install && cd /workspaces
